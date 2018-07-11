6dce91696151433afb563cffbd22259cbbf0e2a5
William Irwin-Harris <w.irwinharris@gmail.com>
2018-07-11 11:44:06 +0800

    Tidy up OCR code

--- ocr.py
+++ ocr.py
@@ -17,40 +17,40 @@ import argparse
 
 # -*- coding: utf-8 -*-
 
-if sys.stdout.encoding != 'cp850':
-  sys.stdout = codecs.getwriter('cp850')(sys.stdout.buffer, 'strict')
-if sys.stderr.encoding != 'cp850':
-  sys.stderr = codecs.getwriter('cp850')(sys.stderr.buffer, 'strict')
+def ocr(img_name):
+    if os.name == 'nt':
+        pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files (x86)\\Tesseract-OCR\\tesseract'
+    else:
+        pytesseract.pytesseract.tesseract_cmd = '/usr/local/bin/tesseract'
 
+    modimg = cv2.imread(img_name)
+    gray = cv2.cvtColor(modimg, cv2.COLOR_BGR2GRAY)
+    gray = cv2.bitwise_not(gray)
 
-parser = argparse.ArgumentParser()
-parser.add_argument("image", help="Image filename")
-args = parser.parse_args()
-
-
-if os.name == 'nt':
-    pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files (x86)\\Tesseract-OCR\\tesseract'
-else:
-    pytesseract.pytesseract.tesseract_cmd = '/usr/local/bin/tesseract'
-
-img_name = args.image
-
-
-modimg = cv2.imread(img_name)
-gray = cv2.cvtColor(modimg, cv2.COLOR_BGR2GRAY)
-gray = cv2.bitwise_not(gray)
-
-tempfname = "{}.png".format(getpid())
-cv2.imwrite(tempfname, gray)
-
-gray = imgprocess.process(gray)
-
-with open("output.txt", "w", encoding='utf-8') as f:
+    tempfname = '{}.png'.format(getpid())
     try:
-        img = Image.open(tempfname)
-        f.write(pytesseract.image_to_string(img).encode('utf-8').decode('utf-8'))
+        cv2.imwrite(tempfname, gray)
+        gray = imgprocess.process(gray)
+        with Image.open(tempfname) as image:
+            return pytesseract.image_to_string(image)
     except IOError as e:
-        print("Error occurred during OCR")
+        print('Error occurred during OCR')
     finally:
-        img.close()
         remove(tempfname)
+
+def main():
+    if sys.stdout.encoding != 'cp850':
+      sys.stdout = codecs.getwriter('cp850')(sys.stdout.buffer, 'strict')
+    if sys.stderr.encoding != 'cp850':
+      sys.stderr = codecs.getwriter('cp850')(sys.stderr.buffer, 'strict')
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('image', help='Image filename')
+    args = parser.parse_args()
+
+    text = ocr(args.image)
+    with open('output.txt', 'w', encoding='utf-8') as f:
+        f.write(text)
+
+if __name__ == '__main__':
+    main()
